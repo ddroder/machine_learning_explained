a45980126bc04399c4f8ca6538fd8a7c479f78ce
ddroder <ddroder@gmail.com>
2021-12-27 14:35:05 -0500

    added distance metrics

--- ml_from_scratch/supervised/regression/knn_regression.py
+++ ml_from_scratch/supervised/regression/knn_regression.py
@@ -2,9 +2,32 @@ import numpy as np
 
 class knn_regressor:
     def __init__(self):
+        self.steps=f""
         pass
+    def data(self):
+        pass
+    def get_distance(self,metric):
+        VALID_METRICS=['eucledian','manhattan']
+        if metric not in VALID_METRICS:
+            raise ValueError(f"Please select a valid distance metric:{VALID_METRICS}")
+        if metric == "eucledian":
+            return "a"
+    def eucledian_distance(self,p1,p2):
+        euc_distance=np.sqrt(np.sum((p1-p2)**2))
+        if math_breakdown:
+            self.steps+=f"\neucledian distance calc between points ({p1,p2}): {euc_distance}"
+        return euc_distance
+    def manhattan_distance(self,p1,p2):
+        man_distance=np.sum(np.abs(p1,p2))
+        if math_breakdown:
+            self.steps+=f"\nmanhattan distance calc between points ({p1,p2}): {man_distance}"
+        return man_distance
+    
     
 
+        
 
 
-
+if __name__=="__main__":
+    print("im alive")
+    

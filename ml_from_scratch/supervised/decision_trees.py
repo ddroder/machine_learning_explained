c540ca162da771e4328c15eeb8df5a8d832848c6
ddroder <ddroder@gmail.com>
2021-12-29 16:09:35 -0500

    added helper classes for tree traversal

--- ml_from_scratch/supervised/decision_trees.py
+++ ml_from_scratch/supervised/decision_trees.py
@@ -1,64 +1,69 @@
-import numpy as np 
 from collections import Counter
+import numpy as np 
 
+
+def entropy(y):
+    hist=np.bincount(y) #num occurances of all class labels
+    ps=hist / len(y)
+    return -np.sum([p*np.log2(p) for p in ps if p > 0]) #need to check if less than zero since log for negative numbers in nan
+
+class node:
+    def __init__(self,feature=None,threshold=None,left=None,right=None,*,value=None) -> None:
+        self.feature=feature
+        self.threshold=threshold
+        self.left=left
+        self.value=value
+        pass
+    def is_leafnode(self):
+        return self.value is not None
 class decision_tree:
-    def __init__(self) -> None:
-        self.help=f"Decision trees are "
-    def gini_index(self,groups,classes):
-        n_instances=float(sum([len(group) for group in groups])) #total num of items in list 
-        gini=0.0
-        for group in groups:
-            size=float(len(group))
-            if size == 0: 
-                continue
-            score = 0.0
-            for class_val in classes:
-                p=[row[-1] for row in group].count(class_val)/size
-                for row in group:
-                    item=[row[-1]].count(class_val)/size
-                score +=p*p
-            gini += (1.0-score)*(size/n_instances)
-        return gini
-
-
-    def test_split(self,index, value, dataset):
-        left, right = list(), list()
-        for row in dataset:
-            if row[index] < value:
-                left.append(row)
-            else:
-                right.append(row)
-        return left, right
-
+    def __init__(self,min_samples=2,max_depth=100,n_feats=None) -> None:
+        self.min_samples=min_samples
+        self.max_depth=max_depth
+        self.n_feats=n_feats
+        self.root=None
+    def fit(self,x,y):
+        #grow the tree out
+        self.n_feats=x.shape[1] if not self.n_feats else min(self.n_feats,x.shape[1]) #make sure x is never bigger than n feats
+        self.root=self._grow_tree(x,y)
+        pass
+    def predict(self,x):
+        #traverse tree
+        pass
+    def _grow_tree(self,x,y,depth=0):
+        n_samples,n_features=x.shape
+        n_labels=len(np.unique(y))
         
-    def get_split(self,dataset):
-        class_values = list(set(row[-1] for row in dataset))
-        b_index, b_value, b_score, b_groups = 999, 999, 999, None
-        for index in range(len(dataset[0])-1):
-            for row in dataset:
-                groups = self.test_split(index, row[index], dataset)
-                gini = self.gini_index(groups, class_values)
-                print('X%d < %.3f Gini=%.3f' % ((index+1), row[index], gini))
-                if gini < b_score:
-                    b_index, b_value, b_score, b_groups = index, row[index], gini, groups
-        return {'index':b_index, 'value':b_value, 'groups':b_groups}
-    def sparse(self):
-        print("im alive")
+        #stopping criteria
+        if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples):
+            leaf_value=self.most_common_label(y)
+            return node(value=leaf_value)
+        feat_idxs=np.random.choice(n_features,self.n_feats,replace=False)
+
+        #greedy search of tree
+        best_feat,best_thresh=self._best_criteria(x,y,feat_idxs)
+
+    def _best_critera(self,x,y,feat_idxs):
+        best_gain=-1
+        split_idx,split_thresh=None,None
+        for feat_idx in feat_idxs:
+            x_col=x[:,feat_idx]
+            thresholds=np.unique(x_col)
+            for thresh in thresholds:
+                gain=self._information_gain(y,x_col,thresh)
+                if gain > best_gain:
+                    best_gain=gain
+                    split_idx=feat_idx
+                    split_thresh=thresh
+        return split_idx,split_thresh
+
+
+    def _most_common_label(self,y):
+        counter=Counter(y)
+        most_common=counter.most_common(1)[0][0]
+        return most_common
+
+
 if __name__=="__main__":
-    dataset = [[2.771244718,1.784783929,0],
-	[1.728571309,1.169761413,0],
-	[3.678319846,2.81281357,0],
-	[3.961043357,2.61995032,0],
-	[2.999208922,2.209014212,0],
-	[7.497545867,3.162953546,1],
-	[9.00220326,3.339047188,1],
-	[7.444542326,0.476683375,1],
-	[10.12493903,3.234550982,1],
-	[6.642287351,3.319983761,1]]
-    group=[[[1, 1], [1, 0]], [[1, 1], [1, 0]]]
-    group2=[[[1, 0], [1, 0]], [[1, 1], [1, 1]]]
     i=decision_tree()
-    i.gini_index(groups=group,classes=[0,1])
-    i.gini_index(groups=group2,classes=[0,1])
-    # i.test_split()
-    i.get_split(dataset=dataset)
+    print(i.min_samples)

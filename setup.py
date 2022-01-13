d26ef4c2edc1ca0645fc25324b666219cf299519
ddroder <ddroder@gmail.com>
2022-01-12 19:31:50 -0500

    pypi push 2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages,setup
 setup(
-    name='ml_from_scratch',
+    name='ml_but_explained',
     packages=find_packages(),
     version='0.1.0',
     description='A machine learning library where the focus is on you learning not the machine!',
